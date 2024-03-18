8b95a958343d6080e923ea1793fc90c190b35081
Bruno Andrade <bruno.minuk@gmail.com>
2024-03-18 15:55:55 -0300

    nested navigation

--- lib/routes/pages.dart
+++ lib/routes/pages.dart
@@ -1,18 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
 import 'package:go_router_learn/page_0.dart';
 import 'package:go_router_learn/page_1.dart';
 import 'package:go_router_learn/page_2.dart';
+import 'package:go_router_learn/page_3.dart';
 
 import 'routes.dart';
 
+// Create keys for `root` & `section` navigator avoiding unnecessary rebuilds
+final _rootNavigatorKey = GlobalKey<NavigatorState>();
+final _sectionNavigatorKey = GlobalKey<NavigatorState>();
 // GoRouter configuration
 final router = GoRouter(
-  initialLocation: Routes.page0,
+  initialLocation: Routes.page3,
+  navigatorKey: _rootNavigatorKey,
   routes: [
     GoRoute(
       path: Routes.page0,
       builder: (context, state) => const Page0(),
     ),
+    StatefulShellRoute.indexedStack(
+        builder: (context, state, navigationShell) => Page3(navigationShell),
+        branches: [
+          StatefulShellBranch(navigatorKey: _sectionNavigatorKey, routes: [
+            GoRoute(
+              path: Routes.page3,
+              builder: (context, state) => const Material(
+                color: Colors.blue,
+              ),
+            ),
+          ]),
+          StatefulShellBranch(routes: [
+            GoRoute(
+              path: '/red',
+              builder: (context, state) => const Material(
+                color: Colors.red,
+              ),
+            ),
+          ])
+        ]),
+    // GoRoute(
+    //   path: Routes.page3,
+    //   builder: (context, state) => const Page3(),
+    // ),
     GoRoute(
         path: Routes.page1,
         builder: (context, state) => const Page1(),
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,5 +1,7 @@
 abstract class Routes {
   static String page0 = '/page0';
+  static String page3 = '/page3';
+
   static String page1 = '/page1';
   static String page2 = 'page2';
 }
